2b9ad57d38cb4e57d57c2a5809d1eab1299f79db
AndrewFlightsim <andrewflightsim.commits@gmail.com>
2021-01-01 18:24:32 +0300

    Add weather source support
    Work on #7

---
+++ Source/FSWeatherInformation.Core/ServiceTypes/IMetarAndTafStringProvider.cs
@@ -0,0 +1,48 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+// ------------------------------------------------------------------------------------------------------
+
+#region Usings
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FSStandardLibrary.Core.Codes;
+using FSWeatherInformation.Core.Statistics;
+using FSWeatherInformation.Core.WeatherStrings;
+using JetBrains.Annotations;
+
+#endregion
+
+namespace FSWeatherInformation.Core.ServiceTypes
+{
+    /// <summary>
+    ///     Interface of weather provider that allows asynchronous downloading of <see cref="MetarAndTafString" /> weather
+    ///     information.
+    /// </summary>
+    public interface IMetarAndTafStringProvider : IMetarStatistics, ITafStatistics
+    {
+        /// <summary>
+        ///     Asynchronously downloads weather information of category <see cref="MetarAndTafString" /> for specified ICAO code.
+        /// </summary>
+        /// <param name="icaoCodeContainer">
+        ///     Class that contain ICAO code of airport to download weather information.
+        /// </param>
+        /// <returns>Task to download <see cref="MetarAndTafString" /> weather information.</returns>
+        [NotNull]
+        Task<MetarAndTafString> DownloadMetarAndTafStringAsync([NotNull] IIcaoCodeContainer icaoCodeContainer);
+
+        /// <summary>
+        ///     Asynchronously downloads weather information of category <see cref="MetarAndTafString" /> for specified ICAO codes.
+        /// </summary>
+        /// <param name="icaoCodeContainers">
+        ///     Collection of classes that contain ICAO codes of airports to download weather information.
+        /// </param>
+        /// <returns>
+        ///     Task to download the list of < see cref="List {T} " /> weather information elements according to
+        ///     specified ICAO codes of airports, where <c> T </c> - < see cref="MetarAndTafString " />.
+        /// </returns>
+        [NotNull]
+        Task<List<MetarAndTafString>> DownloadSeveralMetarAndTafStringsAsync(
+            [NotNull] IReadOnlyList<IIcaoCodeContainer> icaoCodeContainers);
+    }
+}
---
+++ Source/FSWeatherInformation.UnitTests/Tests/Statistics/InformationStatisticsTests.cs
@@ -0,0 +1,44 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+// ------------------------------------------------------------------------------------------------------
+
+#region Usings
+
+using FluentAssertions;
+using FSWeatherInformation.Core.Statistics;
+using FSWeatherInformation.Core.WeatherStrings;
+using NUnit.Framework;
+
+#endregion
+
+namespace FSWeatherInformation.UnitTests.Tests.Statistics
+{
+    [Category("Unit")]
+    [TestFixture]
+    public class InformationStatisticsTests
+    {
+        [Test]
+        public void Constructor_ShouldProcessInformationTypeCorrectly()
+        {
+            //Arrange & act
+            InformationStatistics<MetarString> testStatistics = new InformationStatistics<MetarString>();
+
+            //Assert
+            testStatistics.StatisticsForType.Should().Be(typeof(MetarString));
+        }
+
+        [Test]
+        public void SetValueOfNumberOfRequests()
+        {
+            //Arrange & act
+            InformationStatistics<object> testStatistics = new InformationStatistics<object>();
+            int initialValue = testStatistics.NumberOfRequestsToDownloadInformation;
+            int expectedValue = 9;
+            testStatistics.NumberOfRequestsToDownloadInformation = expectedValue;
+
+            //Assert
+            initialValue.Should().Be(0);
+            testStatistics.NumberOfRequestsToDownloadInformation.Should().Be(expectedValue);
+        }
+    }
+}
